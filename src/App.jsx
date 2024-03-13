681eee89ab856674dbc8c71096c6d4cc21351750
droid-math <ma.silveira844@gmail.com>
2024-03-13 20:18:48 -0300

    Alterações e adição de grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import { Card } from 'primereact/card';
+import React, { useEffect, useState } from 'react';
+import { TaskService } from './service/Tasks';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Menu } from 'primereact/menu';
 import { Panel } from 'primereact/panel';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
 
 import 'primereact/resources/themes/soho-light/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -14,11 +16,7 @@ import './App.css';
 
 
 const App = () => {
-  const [data, setData] = useState([
-    { id: 1, name: 'Item 1', description: 'Descrição do Item 1' },
-    { id: 2, name: 'Item 2', description: 'Descrição do Item 2' },
-    { id: 3, name: 'Item 3', description: 'Descrição do Item 3' },
-  ]);
+  const [tasks, setTasks] = useState([])
   const [items, setItems] = useState([
     {
       label: 'Dashboard',
@@ -33,40 +31,65 @@ const App = () => {
       icon: 'pi pi-fw pi-sign-out',
     },
   ]);
+  useEffect(() => {
+    setTasks(TaskService.getTasksData());
+  }, []);
+
+  const formatCurrency = (value) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  };
+
+  const priceBodyTemplate = (task) => {
+    return formatCurrency(task.price);
+  };
+
+  const ratingBodyTemplate = (task) => {
+    return <Rating value={task.rating} readOnly cancel={false} />;
+  };
+
+  const statusBodyTemplate = (task) => {
+      return <Tag value={task.inventoryStatus} severity={getSeverity(task)}></Tag>;
+  };
+
+  const header = (
+    <div className="align-items-center justify-content-between">
+       <Button label="Novo" icon="pi pi-plus" size="small" />
+    </div>
+  );
+  const footer = `In total there are ${tasks ? tasks.length : 0} products.`;
+
+  const getSeverity = (product) => {
+    switch (product.inventoryStatus) {
+        case 'INSTOCK':
+            return 'success';
+
+        case 'LOWSTOCK':
+            return 'warning';
+
+        case 'OUTOFSTOCK':
+            return 'danger';
+
+        default:
+            return null;
+    }
+};
 
   const handleEdit = (item) => {
     // Implementar a lógica de edição do item
     console.log('Editar item:', item);
   };
 
-  const columns = [
-    <Column field="name" header="Nome" />,
-    <Column field="description" header="Descrição" />,
-  ];
-
-
   return (
     <div className='app flex'>
-      <Menu model={items} className="w-full mr-4 md:w-8 rem font-semibold text-lg"  />
-      <div>
-        <div className='flex mx-auto'>
-          <Panel header="Header" className='mr-2'>
-            <p className="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </Panel>
-          <Panel header="Header" className='ml-2'>
-            <p className="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </Panel>
-        </div>
+      <Menu model={items} className="w-full mr-4 md:w-12 rem font-semibold text-lg"  />
+      <div className='max-h-2rem'>
+        <DataTable className='border-round-sm' size='normal' value={tasks} rows={10} paginator header={header} footer={footer} scrollable={true} tableStyle={{ minWidth: '60rem' }}>
+          <Column field="name" header="Name"></Column>
+          <Column field="price" header="Price" body={priceBodyTemplate}></Column>
+          <Column field="category" header="Category"></Column>
+          <Column field="rating" header="Reviews" body={ratingBodyTemplate}></Column>
+          <Column header="Status" body={statusBodyTemplate}></Column>
+      </DataTable>
       </div>
     </div>
   );
