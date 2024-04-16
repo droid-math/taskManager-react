da059bf8ce336d3206243b58aa7bda02bd4719f3
droid-math <ma.silveira844@gmail.com>
2024-04-16 19:44:27 -0300

    Corrigido o fechamento da Janela de Nova Tarefa

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -15,6 +15,10 @@ const LeftMenu = () => {
           label: 'Logout',
           icon: 'pi pi-fw pi-sign-out',
         },
+        {
+          label: 'Login',
+          icon: 'pi pi-fw pi-sign-out',
+        },
       ]);
     return (
         <Menu model={items} className="w-full mr-4 md:w-4 rem font-semibold text-lg"  />
